646e80b6c00bcb21397a33b0c4a8a8537e42b9e5
robinetta <alex@alexrobinett.com>
2022-04-18 11:13:37 -0400

    Fisrt commit with base code

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#newHeadline').addEventListener('click', changeHeadline)
+
+function changeHeadline(){
+    let newHeadline = document.querySelector('input').value
+
+    document.querySelector('#headline').innerText = newHeadline
+}
+
